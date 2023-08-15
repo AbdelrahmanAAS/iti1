af02a0542f8b96e86c4a73e4a94c422b651ad7d5
abdelrahman3300 <abdelrahman.alaa1@msa.edu.eg>
2023-08-15 17:35:47 +0300

    three

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about-us.html', function () {
+    return view('about-us');
+});
+
+Route::get('/blog.html', function () {
+    return view('blog');
+});
+
+Route::get('/contact.html', function () {
+    return view('contact');
+});
+
+Route::get('/departments.html', function () {
+    return view('departments');
+});
+
+Route::get('/doctors.html', function () {
+    return view('doctors');
+});
+
+Route::get('/elements.html', function () {
+    return view('elements');
+});
+
+Route::get('/index.html', function () {
+    return view('index');
+});
+
+Route::get('/single-blog.html', function () {
+    return view('single-blog');
 });
